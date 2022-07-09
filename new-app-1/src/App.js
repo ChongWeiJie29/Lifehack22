82607e7cdf88ce25b83c8b8114afd146c297db47
WeiJie2909 <wei.cho@handshakes.com.sg>
2022-07-09 17:18:55 +0800

    test

--- new-app-1/src/App.js
+++ new-app-1/src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import './pages/homepage'
-import Homepage from './pages/homepage';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
     </div>
   );
 }
