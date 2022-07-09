709de6a8814291112704c5396e4b2617134a7c64
Nicholas <nicholastanyunyu@gmail.com>
2022-07-10 04:28:20 +0800

    css

--- new-app-1/src/pages/reset/reset.js
+++ new-app-1/src/pages/reset/reset.js
@@ -1,13 +1,14 @@
+import "../../styles/reset.css";
 function Reset() {
-    return (
-      <div className="App">
-        <h2>Email</h2>
-        <p>Enter your email to reset password.</p>
-        <input type="textbox" placeholder="Email Address"/>
-        <br></br>
-        <button>Send Link</button>
-      </div>
-    );
-  }
-  
-  export default Reset;
+  return (
+    <div className="App">
+      <h2 id="email_title">Email</h2>
+      <p>Enter your email to reset password.</p>
+      <input type="textbox" placeholder="Email Address" />
+      <br></br>
+      <button>Send Link</button>
+    </div>
+  );
+}
+
+export default Reset;
