ab3ad11ac57d62a27c0364fda75e7bf49cbe0832
WeiJie2909 <wei.cho@handshakes.com.sg>
2022-07-10 01:44:33 +0800

    a

--- new-app-1/src/pages/homepage/modal.js
+++ new-app-1/src/pages/homepage/modal.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Itemslist from '../itemslist/itemslist';
-import getUsername  from '../../functions/accessDB';
 
 function Modal(props) {
     const [userName, setUserName] = useState('');
