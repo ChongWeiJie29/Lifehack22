37d14c75bf55b56db591c1468155f59c5c59625a
Nicholas <nicholastanyunyu@gmail.com>
2022-07-10 03:51:59 +0800

    stuff

--- new-app-1/src/App.js
+++ new-app-1/src/App.js
@@ -1,4 +1,4 @@
-import Homepage from "./pages/homepage/homepage"; 
+import Homepage from "./pages/homepage/homepage";
 import Scanner from "./pages/qr/scanner";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Reset from "./pages/reset/reset";
@@ -7,14 +7,14 @@ import Itemslist from "./pages/itemslist/itemslist";
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="scanner" element={<Scanner />} />
-        <Route path="reset" element={<Reset />} />
-        <Route path="itemslist" element={<Itemslist />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="scanner" element={<Scanner />} />
+          <Route path="reset" element={<Reset />} />
+          <Route path="itemslist" element={<Itemslist />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- new-app-1/src/pages/itemslist/itemslist.js
+++ new-app-1/src/pages/itemslist/itemslist.js
@@ -1,31 +1,44 @@
+import "../../styles/itemlist.css";
 import Modal from "./modal_filter";
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Scanner from '../qr/scanner';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Scanner from "../qr/scanner";
 
 function Itemslist() {
-    const [modalState, setModalState] = useState("none");
-    return (
-      <div className="App">
-        <h1>Items</h1>
-        <button id="myBtn" onClick={() => setModalState("block")}>
+  const [modalState, setModalState] = useState("none");
+  return (
+    <div className="App">
+      <div id="header_list">
+        <h1 id="item_title">Items</h1>
+        <button id="Filter_button" onClick={() => setModalState("block")}>
           Filter
         </button>
         <Modal display={modalState} setDisplayState={setModalState} />
-        <h2>Item</h2>
+      </div>
+      <div class="item">
+        <h2>Item0</h2>
         <p>Expiry Date: 01-Jan-2023</p>
+      </div>
+      <div class="item">
         <h2>Item1</h2>
         <p>Expiry Date: 02-Jan-2023</p>
+      </div>
+      <div class="item">
         <h2>Item2</h2>
         <p>Expiry Date: 03-Jan-2023</p>
+      </div>
+      <div class="item">
         <h2>Item3</h2>
         <p>Expiry Date: 04-Jan-2023</p>
-        <div class="tab">
-            <button class="tablinks">Items</button>
-            <Link to="../scanner"><button class="tablinks">QR code scanner</button></Link>
-        </div>
       </div>
-    );
-  }
-  
-  export default Itemslist;
+      <div class="tab">
+        <button class="tablinks">Items</button>
+        <Link to="../scanner">
+          <button class="tablinks">QR code scanner</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Itemslist;
