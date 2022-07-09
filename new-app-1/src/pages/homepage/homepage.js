b33304310b1a3495347b16efe79482b89bb33037
Nicholas <nicholastanyunyu@gmail.com>
2022-07-09 20:50:40 +0800

    Update homepage.js

--- new-app-1/src/pages/homepage/homepage.js
+++ new-app-1/src/pages/homepage/homepage.js
@@ -1,15 +1,19 @@
-import '../../styles/homepage.css'
-import Modal from './modal';
-import React, { useState } from 'react';
+import "../../styles/homepage.css";
+import Modal from "./modal";
+import React, { useState } from "react";
 
 function Homepage() {
   const [modalState, setModalState] = useState("none");
   return (
     <div className="App">
-        <h1>INSERT NAME</h1>
-        <p>Smarter meal planning decisions</p>
-        <button id="myBtn" onClick={() => setModalState("block")}>Log in</button>
-        <Modal display={modalState} setDisplayState={setModalState} />
+      <h1 id="home_title">EX-(INS)PIRE</h1>
+      <p id="home_description">Monitor your food storage</p>
+      <p id="home_description">Track expiry dates</p>
+      <p id="home_description">No more food waste</p>
+      <button id="myBtn" onClick={() => setModalState("block")}>
+        Log in
+      </button>
+      <Modal display={modalState} setDisplayState={setModalState} />
     </div>
   );
 }
