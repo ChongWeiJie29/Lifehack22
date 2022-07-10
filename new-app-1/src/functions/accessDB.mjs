29445596031bb242fecf94a7a901c4263a94f91c
WeiJie2909 <wei.cho@handshakes.com.sg>
2022-07-10 11:16:55 +0800

    tset

--- new-app-1/src/functions/accessDB.mjs
+++ new-app-1/src/functions/accessDB.mjs
@@ -1,19 +1,17 @@
-import initSqlJs from 'sql.js';
-import { readFileSync } from 'fs';
-import fileUrl from 'file-url';
+
 
 export async function checkUserName(username) {
-    let correct = false;
-    
-    const SQL = await initSqlJs();
-    const buf = readFileSync(fileUrl('../database/LifeHack.db/'));
-    const db = new SQL.Database(new Uint8Array(buf));
-    
-    const query = db.exec(`SELECT * FROM users WHERE username = '${username}';`);
-    if (query[0]) {
-        correct = true
-    } else {
-        correct = false
-    }
-    return correct;
+    fetch('../database/users.json').then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    })
+    .then(json => {
+        this.users = json;
+        console.log(this.users);
+    })
+    .catch(function () {
+        this.dataError = true;
+    })
 }
