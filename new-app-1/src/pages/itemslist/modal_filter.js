2233342adb435b18f2d2a065083c48d6ed007255
Nicholas <nicholastanyunyu@gmail.com>
2022-07-10 09:38:04 +0800

    css changes

--- new-app-1/src/pages/itemslist/modal_filter.js
+++ new-app-1/src/pages/itemslist/modal_filter.js
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Itemslist from '../itemslist/itemslist';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../../styles/modal_filter.css";
+import Itemslist from "../itemslist/itemslist";
 
 function Modal(props) {
-    const [modalState, setModalState] = useState("none");
-    return (
-          <div id="myModal" className="modal" style={{display:props.display}}>
-              <div className="modal-content">
-                  <span className="close" onClick={()=> props.setDisplayState('none')}>&times;</span>
-                  <h2>Please select how you want to view</h2>
-                  <input type="radio" id="expiry"/>
-                  <label for="expiry">Expiry Date</label>
-                  <br></br>
-                  <input type="radio" id="name" />
-                  <label for="name">Alphabetical Order</label>
-                  <br></br>
-                  <Link to='./itemslist'><button>Confirm</button></Link>
-              </div>
-        </div>
-    );
-  }
-  
-  export default Modal;
+  const [modalState, setModalState] = useState("none");
+  return (
+    <div id="filter_modal" className="modal" style={{ display: props.display }}>
+      <div className="modal-content">
+        <span className="close" onClick={() => props.setDisplayState("none")}>
+          &times;
+        </span>
+        <h2 id="modal_filter_title">Sort by:</h2>
+        <input type="radio" id="expiry" />
+        <label for="expiry" class="filter_options">
+          Expiry Date
+        </label>
+        <br></br>
+        <input type="radio" id="name" />
+        <label for="name" class="filter_options">
+          Alphabetical Order
+        </label>
+        <br></br>
+        <Link to="./itemslist">
+          <button>Confirm</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
