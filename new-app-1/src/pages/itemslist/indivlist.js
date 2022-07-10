539f9253e8c06e90f0ee3fe8c34aee1e1822d9ce
WeiJie2909 <wei.cho@handshakes.com.sg>
2022-07-10 11:56:52 +0800

    final

---
+++ new-app-1/src/pages/itemslist/indivlist.js
@@ -0,0 +1,12 @@
+
+
+function IndivItems(props) {
+    return (
+          <div class="item">
+            <h2>{props.item}</h2>
+            <p>{props.exiry}</p>
+          </div>
+    );
+  }
+  
+  export default IndivItems;
